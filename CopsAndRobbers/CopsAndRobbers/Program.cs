b748afcf0766012f609ff7f4665b999bfcdea210
Oscar Lejon <oscar.lejon@campusnykoping.se>
2024-10-23 11:25:50 +0200

    Added a test feature

--- CopsAndRobbers/CopsAndRobbers/Program.cs
+++ CopsAndRobbers/CopsAndRobbers/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Test");
             List<People> peoples = new List<People>();
             peoples.Add (new Citizen("Oscar", 1, 1 , 1 ,1));
             peoples.Add(new Citizen("Emil", 1, 1, 1, 1));
