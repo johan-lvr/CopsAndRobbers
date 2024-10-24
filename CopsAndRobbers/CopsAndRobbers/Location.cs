6118efad0d5b116a7faae25f5ed2828f8f95ee19
Emil Berg <emil94berg@gmail.com>
2024-10-24 12:28:59 +0200

    Added Display method for City and People

---
+++ CopsAndRobbers/CopsAndRobbers/Location.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopsAndRobbers
+{
+    internal class Location
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public List<string> News { get; set; }
+        public List<Robber> Prisoners { get; set; }
+        public List<People> Peoples { get; set; }
+        
+        public int[,] CityGrid { get; set; }
+
+        public Location(int height, int width)
+        {
+            Height = height;
+            Width = width;
+            CityGrid = new int[height, width];
+            Peoples = new List<People>();
+            //DisplayLocation();
+        }
+        public void DisplayPeople()
+        {
+            for (int i = 0; i < Peoples.Count; i++)
+            {
+                Console.SetCursorPosition(Peoples[i].PosX, Peoples[i].PosY);
+                if (Peoples[i] is Citizen)
+                {
+                    Console.Write("C");
+                }
+                else if (Peoples[i] is Robber)
+                {
+                    Console.Write("R");
+                }
+                else
+                {
+                    Console.Write("P");
+                }
+            }
+        }
+
+        
+
+        public void DisplayLocation()
+        {
+            
+            for (int col = 0; col <= Height; col++)
+            {
+                for (int row = 0; row <= Width; row++)
+                {
+
+                    if (col == 0 || col == Height || row == 0 || row == Width)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+
+                }
+                Console.WriteLine();
+            }
+        }
+       
+    }
+    class City: Location
+    {
+        public int AmmountOfCitizen { get; }
+        public int AmmountOfTheifs { get; }
+        public int AmmountOfCops { get; }
+        public City(int ammountOfCitizen, int ammountOfTheifs, int ammountOfCops, int height, int width) : base(height, width)
+        {
+            CreatePeople(Peoples, ammountOfCitizen, ammountOfTheifs, ammountOfCops);
+            // InitCityGrid();
+        }
+        private void InitCityGrid()
+        {
+            for (int i = 0; i < Peoples.Count; i++)
+            {
+                CityGrid[Peoples[i].PosX, Peoples[i].PosY] = i;
+            }
+        }
+        private void CreatePeople(List<People> peoples, int ammountOfCitizen, int ammountOfTheifs, int ammountOfCops)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < ammountOfCitizen; i++)
+            {
+                peoples.Add(new Citizen($"Medbergoare{i}", rnd.Next(1, Width), rnd.Next(1, Height), rnd.Next(-2, 2), rnd.Next(-2, 2)));
+            }
+            for (int i = 0; i < ammountOfTheifs; i++)
+            {
+                peoples.Add(new Robber($"Tjuv{i}", rnd.Next(1, Width), rnd.Next(1, Height), rnd.Next(-2, 2), rnd.Next(-2, 2)));
+            }
+            for (int i = 0; i < ammountOfCops; i++)
+            {
+                peoples.Add(new Cop($"Polis{i}", rnd.Next(1, Width), rnd.Next(1, Height), rnd.Next(-2, 2), rnd.Next(-2, 2)));
+            }
+        }
+
+    }
+    class Prison: Location
+    {
+        public Prison(int height, int width) : base (width, height)
+        {
+            
+        }
+    }
+}
--- CopsAndRobbers/CopsAndRobbers/Program.cs
+++ CopsAndRobbers/CopsAndRobbers/Program.cs
@@ -4,22 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
-            List<People> peoples = new List<People>();
-            peoples.Add (new Citizen("Oscar", 1, 1 , 1 ,1));
-            peoples.Add(new Citizen("Emil", 1, 1, 1, 1));
-            peoples.Add(new Citizen("Johan", 1, 1, 1, 1));
-            peoples.Add(new Citizen("Emil 2", 1, 1, 1, 1));
+            //Console.WriteLine("Test");
+            //List<People> peoples = new List<People>();
+            //peoples.Add (new Citizen("Oscar", 1, 1 , 1 ,1));
+            //peoples.Add(new Citizen("Emil", 1, 1, 1, 1));
+            //peoples.Add(new Citizen("Johan", 1, 1, 1, 1));
+            //peoples.Add(new Citizen("Emil 2", 1, 1, 1, 1));
 
-            foreach (People people in peoples)
-            {
-                Console.WriteLine(people.Name + " ");
-                foreach (Goods goods in people.Goods)
-                {
-                    Console.Write(goods.ItemName + " ");
-                }
-            }
-            Console.ReadLine();
+            //foreach (People people in peoples)
+            //{
+            //    Console.WriteLine(people.Name + " ");
+            //    foreach (Goods goods in people.Goods)
+            //    {
+            //        Console.Write(goods.ItemName + " ");
+            //    }
+            //}
+            //Console.ReadLine();
+
+            City City = new City(12,12,12,20,50);
+            City.DisplayLocation();
+            City.DisplayPeople();
+            Console.ReadKey();
         }
     }
 }
